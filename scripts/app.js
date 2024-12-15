4e266c9fae0fcf5b2f5a11d7db812146d533f802
abdalla-shaker <abdallashaker143@gmail.com>
2024-12-15 09:57:04 +0200

    adding carousel images

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,36 @@
+const images = document.querySelectorAll(".carousel-image");
+const dots = document.querySelectorAll(".carousel-dot");
+let currentIndex = 0;
+const intervalTime = 4000;
+
+const updateCarousel = () => {
+  images.forEach((img, index) => {
+    img.classList.remove("active");
+    dots[index].classList.remove("active");
+  });
+
+  if (currentIndex >= 0 && currentIndex < images.length) {
+    images[currentIndex].classList.add("active");
+    dots[currentIndex].classList.add("active");
+  }
+};
+
+const showNextImage = () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateCarousel();
+};
+
+updateCarousel();
+
+setInterval(showNextImage, intervalTime);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentIndex = index;
+    updateCarousel();
+  });
+});
+
 const navIcon = document.getElementById("nav-icon");
 
 const togglingClasses = (element, classes) => {
