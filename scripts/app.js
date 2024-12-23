ed81dbced83716ae021fc3cad808d436e2767c34
abdalla-shaker <abdallashaker143@gmail.com>
2024-12-23 16:43:56 +0200

    adding who are we page

--- scripts/app.js
+++ scripts/app.js
@@ -30,13 +30,3 @@ dots.forEach((dot, index) => {
     updateCarousel();
   });
 });
-
-const navIcon = document.getElementById("nav-icon");
-
-const togglingClasses = (element, classes) => {
-  element.classList.toggle(classes);
-};
-
-navIcon.addEventListener("click", () => {
-  togglingClasses(navIcon, "open");
-});
